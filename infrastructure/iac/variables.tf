774da982f54367cf6aea7b290c0be28a25e46685
lucasfreitasmoura <lucasdefreitasmoura@gmail.com>
2021-08-14 15:41:01 -0300

    primeiro commit

---
+++ infrastructure/iac/variables.tf
@@ -0,0 +1,13 @@
+variable "base_bucket_name" {
+  default = "datalake-igti-tf"
+}
+
+variable "ambiente" {
+
+  default = "producao"
+}
+
+variable "numero_conta" {
+  default = "757573267027"
+
+}
---
+++ job_spark.py
@@ -0,0 +1,33 @@
+#IMPORTAR ALGUMAS FUNÇÕES
+from pyspark.sql.functions import mean, max, min, col, count
+from pyspark.sql import SparkSession
+
+spark = (
+    SparkSession.builder.appName("ExerciseSpark")
+    .getOrCreate()
+)
+
+# Ler os dados do enem 2019
+
+enem = (
+    spark
+    .read
+    .format("csv")
+    .option("header", True)
+    .option("inferSchema", True)
+    .option("delimiter", ";")
+    .load("s3://datalake-lucas-igti-edc/raw-data/enem/")
+    
+)
+
+#Otimizar o Código utilizando formato Parquet
+
+(
+    enem
+    .write
+    .mode("overwrite")
+    .format("parquet")
+    .partitionBy("year")
+    .save("s3://datalake-lucas-igti-edc/staging/enem")
+    
+)
