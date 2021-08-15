4b607ebce135727ab7c520d45a8d5bdd3bb72aeb
lucasfreitasmoura <lucasdefreitasmoura@gmail.com>
2021-08-14 23:47:44 -0300

    Alteracao de pastar e criacao do terraform state

---
+++ infrastructure/s3.tf
@@ -0,0 +1,18 @@
+resource "aws_s3_bucket" "dl" {
+    bucket = "datalake-lucas-igti-edc-deploy-tf"
+    acl = "private"
+
+    tags = {
+      IES = "IGTI"
+      CURSO = "EDC"
+    }
+
+    server_side_encryption_configuration {
+      rule{
+          apply_server_side_encryption_by_default{
+              sse_algorithm = "AES256"
+          }
+      }
+    }
+  
+}
